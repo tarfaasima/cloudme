e9f1abbacd03f59bb4c98ff160f731791b9a167b
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2009-03-09 22:30:48 +0000

    Testing some XSD stuff. The project is quite dirty now and needs a lot of cleanup.

---
+++ metamodel/trunk/src/test/java/org/cloudme/metamodel/XsdTest.java
@@ -0,0 +1,51 @@
+package org.cloudme.metamodel;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class XsdTest {
+    @Test
+    public void testXsd() throws Exception {
+        SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        ErrorHandler myErrorHandler = new ErrorHandler() {
+            public void warning(SAXParseException exception) throws SAXException {
+                System.out.println("WARNING: " + exception.toString());
+            }
+
+            public void fatalError(SAXParseException exception) throws SAXException {
+                System.out.println("FATAL: " + exception.toString());
+            }
+
+            public void error(SAXParseException exception) throws SAXException {
+                System.out.println("ERROR: " + exception.toString());
+            }
+        };
+        sf.setErrorHandler(myErrorHandler);
+        // read to memory
+        String schemaStr = FileUtils.readFileToString(new File("metamodel.xsd"));
+        System.out.println(schemaStr);
+        // get schema from memory
+        Schema schema = sf.newSchema(new StreamSource(new ByteArrayInputStream(schemaStr.getBytes())));
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        dbf.setSchema(schema);
+        dbf.setValidating(true);
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        db.setErrorHandler(myErrorHandler);
+        Document doc = db.parse(new File("metamodel.xml"));
+    }
+}
