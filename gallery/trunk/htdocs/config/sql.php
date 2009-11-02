a961f2b41c59c262cc3965f75e5f1558097dc211
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2009-11-02 21:27:18 +0000

    Added SQL dump.

--- gallery/trunk/htdocs/config/sql.php
+++ gallery/trunk/htdocs/config/sql.php
@@ -1,5 +1,36 @@
 <?php
 $sql_init = <<< SQL
+-- phpMyAdmin SQL Dump
+-- version 2.11.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Erstellungszeit: 02. November 2009 um 22:26
+-- Server Version: 5.0.41
+-- PHP-Version: 5.2.6
 
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Datenbank: `gallery`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE `user` (
+  `login` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  PRIMARY KEY  (`login`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` VALUES('admin', '21232f297a57a5a743894a0e4a801fc3');
 SQL
 ?>
