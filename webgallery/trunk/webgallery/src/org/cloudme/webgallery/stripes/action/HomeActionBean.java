d28c14d52df8751c2386d6a2ceac85ca63b1736b
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2009-11-28 21:45:00 +0000

    Configuration completed with running Spring, Stripes.

---
+++ webgallery/trunk/webgallery/src/org/cloudme/webgallery/stripes/action/HomeActionBean.java
@@ -0,0 +1,26 @@
+package org.cloudme.webgallery.stripes.action;
+
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.UrlBinding;
+
+@UrlBinding("/photos/home")
+public class HomeActionBean implements ActionBean {
+    private ActionBeanContext context;
+
+    public ActionBeanContext getContext() {
+        return context;
+    }
+
+    public void setContext(ActionBeanContext context) {
+        this.context = context;
+    }
+    
+    @DefaultHandler
+    public Resolution show() {
+        return new ForwardResolution("/WEB-INF/jsps/home.jsp");
+    }
+}
---
+++ webgallery/trunk/webgallery/war/WEB-INF/jsps/home.jsp
@@ -0,0 +1,10 @@
+<%@page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8" %>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>Gallery</title>
+  </head>
+  <body>
+    <h1>Gallery</h1>
+  </body>
+</html>
