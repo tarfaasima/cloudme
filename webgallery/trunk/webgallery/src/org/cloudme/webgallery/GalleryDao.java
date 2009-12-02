d265c73f9addc31c3594c3cd13a2004f10b838a7
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2009-12-02 23:26:15 +0000

    Now persistency with Spring.

--- webgallery/trunk/webgallery/src/org/cloudme/webgallery/GalleryDao.java
+++ webgallery/trunk/webgallery/src/org/cloudme/webgallery/GalleryDao.java
@@ -2,52 +2,35 @@ package org.cloudme.webgallery;
 
 import java.util.Collection;
 
+import javax.jdo.JDOException;
 import javax.jdo.PersistenceManager;
-import javax.jdo.Query;
+import javax.jdo.PersistenceManagerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jdo.JdoCallback;
+import org.springframework.orm.jdo.JdoTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class GalleryDao {
-//    private final JdoTemplate jdoTemplate;
+    private final JdoTemplate jdoTemplate;
 
-//    @Autowired
-//    public GalleryDao(PersistenceManagerFactory pmf) {
-//        jdoTemplate = new JdoTemplate(pmf);
-//    }
+    @Autowired
+    public GalleryDao(PersistenceManagerFactory pmf) {
+        jdoTemplate = new JdoTemplate(pmf);
+    }
     
-//    @Transactional
     public void save(Gallery gallery) {
-        PersistenceManager pm = PMF.get().getPersistenceManager();
-        try {
-            pm.makePersistent(gallery);
-        }
-        finally {
-            pm.close();
-        }
-//        jdoTemplate.makePersistent(gallery);
+        jdoTemplate.makePersistent(gallery);
         System.out.println("saved: " + gallery.getName());
     }
     
     @SuppressWarnings("unchecked")
     public Collection<Gallery> findAll() {
-        PersistenceManager pm = PMF.get().getPersistenceManager();
-        try {
-            Query query = pm.newQuery(Gallery.class);
-            Collection<Gallery> galleries = (Collection<Gallery>) query.execute();
-            return pm.detachCopyAll(galleries);
-        }
-        finally {
-            pm.close();
-        }
-//        return jdoTemplate.executeFind(new JdoCallback<Collection<Gallery>>() {
-//            public Collection<Gallery> doInJdo(PersistenceManager pm) throws JDOException {
-//                Set galleries = pm.getManagedObjects(Gallery.class);
-//                if (galleries == null) {
-//                    return Collections.EMPTY_LIST;
-//                }
-//                return pm.detachCopyAll(galleries);
-//            }
-//        });
+        return jdoTemplate.executeFind(new JdoCallback<Collection<Gallery>>() {
+            public Collection<Gallery> doInJdo(PersistenceManager pm) throws JDOException {
+                return pm.detachCopyAll((Collection<Gallery>) pm.newQuery(Gallery.class).execute());
+            }
+        });
     }
 }
