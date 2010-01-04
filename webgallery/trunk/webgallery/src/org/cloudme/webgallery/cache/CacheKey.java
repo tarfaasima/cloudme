53c5ae8cb971e6778b32d55345bc51ca79911e08
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2010-01-04 16:24:59 +0000

    - Added serial version ID to CacheKey - Updated todo.txt

--- webgallery/trunk/webgallery/src/org/cloudme/webgallery/cache/CacheKey.java
+++ webgallery/trunk/webgallery/src/org/cloudme/webgallery/cache/CacheKey.java
@@ -8,39 +8,40 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 public class CacheKey implements Serializable {
-    private final Serializable[] parameters;
+	private static final long serialVersionUID = 8269975340644775472L;
+	private final Serializable[] parameters;
 
-    public CacheKey(Serializable... parameters) {
-        for (int i = 0; i < parameters.length; i++) {
-            if (parameters[i] == null) {
-                throw new IllegalArgumentException("Parameters must not be null (" + i + ")");
-            }
-        }
-        this.parameters = parameters;
-    }
+	public CacheKey(Serializable... parameters) {
+		for (int i = 0; i < parameters.length; i++) {
+			if (parameters[i] == null) {
+				throw new IllegalArgumentException("Parameters must not be null (" + i + ")");
+			}
+		}
+		this.parameters = parameters;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (obj == this) {
-            return true;
-        }
-        if (obj.getClass() != getClass()) {
-            return false;
-        }
-        CacheKey key = (CacheKey) obj;
-        return new EqualsBuilder().append(parameters, key.parameters).isEquals();
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (obj == this) {
+			return true;
+		}
+		if (obj.getClass() != getClass()) {
+			return false;
+		}
+		CacheKey key = (CacheKey) obj;
+		return new EqualsBuilder().append(parameters, key.parameters).isEquals();
+	}
 
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder().append(parameters).toHashCode();
-    }
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(parameters).toHashCode();
+	}
 
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this).append(parameters).toString();
-    }
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this).append(parameters).toString();
+	}
 }
