3a5e94b2afb303ea78a8ca3d73f232a8fd61ccd3
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2010-02-06 20:39:47 +0000

    - Added log4j logging - Implemented "over quota" default image if transformation cannot be performed.

--- webgallery/trunk/webgallery/src/org/cloudme/webgallery/image/gae/GaeImageService.java
+++ webgallery/trunk/webgallery/src/org/cloudme/webgallery/image/gae/GaeImageService.java
@@ -1,5 +1,10 @@
 package org.cloudme.webgallery.image.gae;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 import org.cloudme.webgallery.image.ContentType;
 import org.cloudme.webgallery.image.ImageFormat;
 import org.cloudme.webgallery.image.ImageService;
@@ -10,9 +15,12 @@ import com.google.appengine.api.images.Image;
 import com.google.appengine.api.images.ImagesService;
 import com.google.appengine.api.images.ImagesServiceFactory;
 import com.google.appengine.api.images.ImagesService.OutputEncoding;
+import com.google.apphosting.api.ApiProxy.OverQuotaException;
 
 @Component
 public class GaeImageService implements ImageService {
+    private static final Logger LOG = Logger.getLogger(GaeImageService.class);
+    
     public byte[] process(byte[] data, ImageFormat format, ContentType type, float balance) {
         Image image = ImagesServiceFactory.makeImage(data);
         CompositeTransform tx = ImagesServiceFactory.makeCompositeTransform();
@@ -27,6 +35,28 @@ public class GaeImageService implements ImageService {
         tx.concatenate(ImagesServiceFactory.makeResize(format.getWidth(), format.getHeight()));
         ImagesService imagesService = ImagesServiceFactory.getImagesService();
         OutputEncoding outputEncoding = OutputEncoding.valueOf(type.name());
-        return imagesService.applyTransform(tx, image, outputEncoding).getImageData();
+        try {
+            return imagesService.applyTransform(tx, image, outputEncoding).getImageData();
+        }
+        catch (OverQuotaException e) {
+            LOG.warn("Over quota.", e);
+            return getOverQuotaImage(format);
+        }
+    }
+
+    private byte[] getOverQuotaImage(ImageFormat format) {
+        int max = Math.max(format.getWidth(), format.getHeight());
+        String overQuotaFileName = "over_quota_" + max + ".jpg";
+        try {
+            InputStream in = getClass().getResourceAsStream(overQuotaFileName);
+            if (in == null) {
+                in = getClass().getResourceAsStream("over_quota_198.jpg");
+            }
+            return IOUtils.toByteArray(in);
+        }
+        catch (IOException e) {
+            LOG.debug(e);
+            return null;
+        }
     }
 }
--- webgallery/trunk/webgallery/src/org/cloudme/webgallery/service/PhotoService.java
+++ webgallery/trunk/webgallery/src/org/cloudme/webgallery/service/PhotoService.java
@@ -1,10 +1,10 @@
 package org.cloudme.webgallery.service;
 
 import java.util.Collection;
-import java.util.logging.Logger;
 
 import javax.cache.CacheException;
 
+import org.apache.log4j.Logger;
 import org.cloudme.webgallery.cache.CacheService;
 import org.cloudme.webgallery.model.Album;
 import org.cloudme.webgallery.model.Photo;
@@ -17,89 +17,89 @@ import com.google.apphosting.api.ApiProxy.RequestTooLargeException;
 
 @Service
 public class PhotoService extends AbstractService<Long, Photo> {
-	private final PhotoRepository photoRepository;
-	@Autowired
-	private PhotoDataService photoDataService;
-	@Autowired
-	private AlbumService albumService;
-	@Autowired
-	private CacheService cacheService;
-	private static final Logger LOGGER = Logger.getLogger(PhotoService.class.getName());
+    private final PhotoRepository photoRepository;
+    @Autowired
+    private PhotoDataService photoDataService;
+    @Autowired
+    private AlbumService albumService;
+    @Autowired
+    private CacheService cacheService;
+    private static final Logger LOG = Logger.getLogger(PhotoService.class);
 
-	@Autowired
-	protected PhotoService(PhotoRepository repository) throws CacheException {
-		super(repository);
-		photoRepository = repository;
-	}
+    @Autowired
+    protected PhotoService(PhotoRepository repository) throws CacheException {
+        super(repository);
+        photoRepository = repository;
+    }
 
-	/**
-	 * Returns all {@link Photo}s accociated with the given {@link Album} ID.
-	 * 
-	 * @param albumId
-	 *            The ID of the {@link Album}.
-	 * @return The {@link Photo}s, that are associated with the {@link Album}.
-	 */
-	public Collection<Photo> findByAlbumId(Long albumId) {
-		return photoRepository.findByAlbumId(albumId);
-	}
+    /**
+     * Returns all {@link Photo}s accociated with the given {@link Album} ID.
+     * 
+     * @param albumId
+     *            The ID of the {@link Album}.
+     * @return The {@link Photo}s, that are associated with the {@link Album}.
+     */
+    public Collection<Photo> findByAlbumId(Long albumId) {
+        return photoRepository.findByAlbumId(albumId);
+    }
 
-	/**
-	 * Saves the {@link Photo}s for the given {@link Album} ID. If
-	 * {@link Photo#getPhotoData()} is not null, then the {@link PhotoData} is
-	 * saved. If the {@link PhotoData} is too large, the {@link Photo} will not
-	 * be saved. The {@link Album#getPhotoCount()} will be updated.
-	 * 
-	 * @param albumId
-	 *            The ID of the {@link Album}.
-	 * @param photos
-	 *            The {@link Photo}s that are saved.
-	 */
-	public void save(Long albumId, Collection<Photo> photos) {
-		for (Photo photo : photos) {
-			photo.setAlbumId(albumId);
-			save(photo);
-			PhotoData photoData = photo.getPhotoData();
-			if (photoData != null) {
-				photoData.setId(photo.getId());
-				try {
-					photoDataService.save(photoData);
-				} catch (RequestTooLargeException e) {
-					LOGGER.warning("Photo data too large: " + photo.getFileName() + " ("
-							+ photoData.getDataAsArray().length + " bytes)");
-					super.delete(photo.getId());
-				}
-			}
-		}
-		updatePhotoCount(albumId);
-		cacheService.invalidate();
-	}
+    /**
+     * Saves the {@link Photo}s for the given {@link Album} ID. If
+     * {@link Photo#getPhotoData()} is not null, then the {@link PhotoData} is
+     * saved. If the {@link PhotoData} is too large, the {@link Photo} will not
+     * be saved. The {@link Album#getPhotoCount()} will be updated.
+     * 
+     * @param albumId
+     *            The ID of the {@link Album}.
+     * @param photos
+     *            The {@link Photo}s that are saved.
+     */
+    public void save(Long albumId, Collection<Photo> photos) {
+        for (Photo photo : photos) {
+            photo.setAlbumId(albumId);
+            save(photo);
+            PhotoData photoData = photo.getPhotoData();
+            if (photoData != null) {
+                photoData.setId(photo.getId());
+                try {
+                    photoDataService.save(photoData);
+                }
+                catch (RequestTooLargeException e) {
+                    LOG.warn("Photo data too large: " + photo.getFileName() + " (" + photoData.getDataAsArray().length + " bytes)", e);
+                    super.delete(photo.getId());
+                }
+            }
+        }
+        updatePhotoCount(albumId);
+        cacheService.invalidate();
+    }
 
-	private void updatePhotoCount(Long albumId) {
-		int count = photoRepository.countPhotosByAlbumId(albumId);
-		albumService.updatePhotoCount(albumId, count);
-	}
+    private void updatePhotoCount(Long albumId) {
+        int count = photoRepository.countPhotosByAlbumId(albumId);
+        albumService.updatePhotoCount(albumId, count);
+    }
 
-	/**
-	 * Deletes the {@link Photo} and the {@link PhotoData}. If the
-	 * {@link PhotoData} cannot be deleted, the {@link Photo} will not be
-	 * deleted.
-	 */
-	@Override
-	public void delete(Long id) {
-		photoDataService.delete(id);
-		super.delete(id);
-	}
+    /**
+     * Deletes the {@link Photo} and the {@link PhotoData}. If the
+     * {@link PhotoData} cannot be deleted, the {@link Photo} will not be
+     * deleted.
+     */
+    @Override
+    public void delete(Long id) {
+        photoDataService.delete(id);
+        super.delete(id);
+    }
 
-	/**
-	 * Deletes the {@link Photo} and updates the {@link Album#getPhotoCount()}.
-	 * 
-	 * @param albumId
-	 *            The ID of the {@link Album} of the {@link Photo}.
-	 * @param photoId
-	 *            The ID of the {@link Photo}.
-	 */
-	public void delete(Long albumId, Long photoId) {
-		delete(photoId);
-		updatePhotoCount(albumId);
-	}
+    /**
+     * Deletes the {@link Photo} and updates the {@link Album#getPhotoCount()}.
+     * 
+     * @param albumId
+     *            The ID of the {@link Album} of the {@link Photo}.
+     * @param photoId
+     *            The ID of the {@link Photo}.
+     */
+    public void delete(Long albumId, Long photoId) {
+        delete(photoId);
+        updatePhotoCount(albumId);
+    }
 }
