21603f09a37c69144172922d0ca19efbadab3676
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2010-02-08 17:42:26 +0000

    Deleted Album, Photo and old repositories.

--- webgallery/trunk/webgallery/src/org/cloudme/webgallery/persistence/OldAlbumRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cloudme.webgallery.persistence;
-
-import org.cloudme.webgallery.Album;
-
-@Deprecated
-public interface OldAlbumRepository extends Repository<String, Album> {
-}
--- webgallery/trunk/webgallery/src/org/cloudme/webgallery/persistence/OldPhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.cloudme.webgallery.persistence;
-
-import org.cloudme.webgallery.Photo;
-
-@Deprecated
-public interface OldPhotoRepository extends Repository<String, Photo> {
-}
