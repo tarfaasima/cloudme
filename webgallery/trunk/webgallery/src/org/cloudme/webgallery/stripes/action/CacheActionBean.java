3bbbf2880fec735d8e980eebf14e605c5cce6075
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2010-02-25 21:21:11 +0000

    For learning: logging header information.

--- webgallery/trunk/webgallery/src/org/cloudme/webgallery/stripes/action/CacheActionBean.java
+++ webgallery/trunk/webgallery/src/org/cloudme/webgallery/stripes/action/CacheActionBean.java
@@ -1,5 +1,10 @@
 package org.cloudme.webgallery.stripes.action;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import net.sourceforge.stripes.action.ActionBeanContext;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
@@ -14,8 +19,16 @@ public class CacheActionBean extends AbstractActionBean {
     private CacheUpdateService cacheUpdateService;
     private final long timeout = 20000;
     
+    @SuppressWarnings("unchecked")
     @DefaultHandler
     public Resolution update() {
+        ActionBeanContext context = getContext();
+        HttpServletRequest request = context.getRequest();
+        for (Enumeration<String> names = request.getHeaderNames(); names.hasMoreElements();) {
+            String name = names.nextElement();
+            String header = request.getHeader(name);
+            System.out.println(name + " : " + header);
+        }
         cacheUpdateService.update(timeout);
         return new ForwardResolution("/gallery/home");
     }
