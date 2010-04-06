0aaf636bf0ac1a565e324a35daab49f063a8c8ef
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2010-04-06 06:00:41 +0000

    Minor updates

--- webgallery/trunk/webgallery/src/test/java/org/cloudme/webgallery/tags/FunctionsTest.java
+++ webgallery/trunk/webgallery/src/test/java/org/cloudme/webgallery/tags/FunctionsTest.java
@@ -8,6 +8,6 @@ import org.junit.Test;
 public class FunctionsTest {
     @Test
     public void testAppVersion() {
-        assertEquals("22", Functions.appVersion("test-src/test-appengine-web.xml"));
+        assertEquals("22", Functions.appVersion("src/test/resources/test-appengine-web.xml"));
     }
 }
