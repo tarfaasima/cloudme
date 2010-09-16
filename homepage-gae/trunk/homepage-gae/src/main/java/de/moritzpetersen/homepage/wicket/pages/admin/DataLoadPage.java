037f27f55731e02d4bd2eee662d9c338e1c1ad1a
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2010-09-16 20:15:44 +0000

    slooooooooowly working on data upload functionality.

--- homepage-gae/trunk/homepage-gae/src/main/java/de/moritzpetersen/homepage/wicket/pages/admin/DataLoadPage.java
+++ homepage-gae/trunk/homepage-gae/src/main/java/de/moritzpetersen/homepage/wicket/pages/admin/DataLoadPage.java
@@ -1,14 +1,32 @@
 package de.moritzpetersen.homepage.wicket.pages.admin;
 
+import java.io.ByteArrayInputStream;
+
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextArea;
+
+import com.google.inject.Inject;
+
+import de.moritzpetersen.homepage.dataload.DataLoader;
 
 public class DataLoadPage extends WebPage {
+    @Inject
+    private DataLoader dataLoader;
+
+    @SuppressWarnings( "serial" )
     public DataLoadPage() {
-        add(new Form("dataLoadForm") {
+        Form<String> form = new Form<String>("dataload");
+        final TextArea<String> data = new TextArea<String>("data");
+        form.add(data);
+        form.add(new Button("submit") {
             @Override
-            protected void onSubmit() {
+            public void onSubmit() {
+                byte[] bytes = data.getModelObject().getBytes();
+                dataLoader.load(new ByteArrayInputStream(bytes));
             }
         });
+        add(form);
     }
 }
