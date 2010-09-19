d3cd0011b8ed9ec2eb7543fa16fe0e4b575ef72c
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2010-09-19 13:11:20 +0000

    Added feedback handling and validation.

--- homepage-gae/trunk/homepage-gae/src/main/java/de/moritzpetersen/homepage/wicket/pages/admin/DataLoadPage.java
+++ homepage-gae/trunk/homepage-gae/src/main/java/de/moritzpetersen/homepage/wicket/pages/admin/DataLoadPage.java
@@ -5,38 +5,39 @@ import java.io.ByteArrayInputStream;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.Model;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
 
 import de.moritzpetersen.homepage.dataload.DataLoader;
+import de.moritzpetersen.homepage.dataload.InvalidDataException;
 
 public class DataLoadPage extends WebPage {
-    private static final Logger LOG = LoggerFactory
-            .getLogger(DataLoadPage.class);
     @Inject
     private DataLoader dataLoader;
 
     @SuppressWarnings( "serial" )
     public DataLoadPage() {
-        LOG.info("dataLoader = {}", dataLoader.getClass().getName());
         Form<String> form = new Form<String>("dataload");
-        final TextArea<String> data = new TextArea<String>("data",
-                new Model<String>());
+        final FormComponent<String> data = new TextArea<String>("data",
+                new Model<String>()).setRequired(true);
         form.add(data);
         form.add(new Button("submit") {
             @Override
             public void onSubmit() {
-                LOG.info("data = {}", data);
                 byte[] bytes = data.getModelObject().getBytes();
-                LOG.info("bytes.length = {}", bytes.length);
-                dataLoader.load(new ByteArrayInputStream(bytes));
-                LOG.info("done.");
+                try {
+                    dataLoader.load(new ByteArrayInputStream(bytes));
+                }
+                catch (InvalidDataException e) {
+                    getSession().error("Invalid data");
+                }
             }
         });
         add(form);
+        add(new FeedbackPanel("feedback"));
     }
 }
