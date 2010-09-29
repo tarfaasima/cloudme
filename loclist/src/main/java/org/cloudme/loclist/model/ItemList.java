f6406eb498d67b56cfec37e9781e68dd49813e87
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2010-09-29 21:24:41 +0000

    Added ItemList.

---
+++ loclist/src/main/java/org/cloudme/loclist/model/ItemList.java
@@ -0,0 +1,39 @@
+package org.cloudme.loclist.model;
+
+import java.util.Date;
+
+import javax.persistence.Id;
+
+import com.googlecode.objectify.annotation.Unindexed;
+
+public class ItemList {
+    @Id
+    private Long id;
+    @Unindexed
+    private String name;
+    private Date lastUpdate;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+}
