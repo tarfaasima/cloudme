afb4ef09a857523bd71c32cb16c4a14d4c818982
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2011-07-05 20:49:24 +0000

    Implementing simple REST service.

---
+++ passwolk/src/main/java/org/cloudme/passwolk/json/JsonWriter.java
@@ -0,0 +1,12 @@
+package org.cloudme.passwolk.json;
+
+import java.io.PrintWriter;
+
+public class JsonWriter {
+    private final PrintWriter out;
+
+    public JsonWriter(PrintWriter out) {
+        this.out = out;
+    }
+
+}
