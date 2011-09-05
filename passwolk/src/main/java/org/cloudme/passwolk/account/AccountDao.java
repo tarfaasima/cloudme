175459ac88bdea6819dbc1be1811af2320b3288c
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2011-09-05 19:00:37 +0000

    Removed old project contents.

--- passwolk/src/main/java/org/cloudme/passwolk/account/AccountDao.java
+++
@@ -1,9 +0,0 @@
-package org.cloudme.passwolk.account;
-
-import org.cloudme.sugar.AbstractDao;
-
-class AccountDao extends AbstractDao<Account> {
-    AccountDao() {
-        super(Account.class);
-    }
-}
