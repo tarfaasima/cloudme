8bbb6af91b9cfe9e8019150061d966345e791ce3
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2011-09-14 20:02:55 +0000

    Updated JavaDoc. Slight internal code improvements.

--- passwolk/src/main/java/org/cloudme/passwolk/servlet/json/JsonWriter.java
+++ passwolk/src/main/java/org/cloudme/passwolk/servlet/json/JsonWriter.java
@@ -1,6 +1,7 @@
 package org.cloudme.passwolk.servlet.json;
 
 import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -17,12 +18,27 @@ import org.apache.commons.beanutils.PropertyUtils;
  */
 public class JsonWriter {
 
+    /**
+     * The internal writer.
+     */
     private final PrintWriter out;
 
+    /**
+     * Initializes the {@link JsonWriter} with the given {@link PrintWriter}.
+     * 
+     * @param out
+     *            The {@link PrintWriter} that is used to write JSON data to.
+     */
     public JsonWriter(PrintWriter out) {
         this.out = out;
     }
 
+    /**
+     * Writes the objects of the given {@link Iterable}.
+     * 
+     * @param iterable
+     *            The input that is transformed to JSON.
+     */
     public void write(Iterable<?> iterable) {
         out.write("[");
         for (Object object : iterable) {
@@ -32,11 +48,16 @@ public class JsonWriter {
         out.flush();
     }
 
-	public void write(Object object) {
-        out.write("{");
-        try {
-            @SuppressWarnings( "unchecked" )
-            Map<String, Object> props = PropertyUtils.describe(object);
+    /**
+     * Writes a single {@link Object}.
+     * 
+     * @param object
+     *            The object that ist transformed to JSON.
+     */
+    public void write(Object object) {
+        Map<String, Object> props = describe(object);
+        if (props != null) {
+            out.write("{");
             boolean isFirst = true;
             for (Entry<String, Object> property : props.entrySet()) {
                 String key = property.getKey();
@@ -64,11 +85,33 @@ public class JsonWriter {
                     }
                 }
             }
+            out.write("}");
         }
-        catch (Exception e) {
-            // ignore
+    }
+
+    /**
+     * Creates a map containing all property names and values of the object.
+     * 
+     * @param object
+     *            The object, that must follow the Java Bean syntax (using
+     *            accessors in getXXX() setXXX style()).
+     * 
+     * @return A map containing the properties.
+     */
+    @SuppressWarnings( "unchecked" )
+    private Map<String, Object> describe(Object object) {
+        try {
+            return PropertyUtils.describe(object);
         }
-        out.write("}");
+        catch (IllegalAccessException e) {
+            return null;
+        }
+        catch (InvocationTargetException e) {
+            return null;
+        }
+        catch (NoSuchMethodException e) {
+            return null;
+        }
     }
 
 }
