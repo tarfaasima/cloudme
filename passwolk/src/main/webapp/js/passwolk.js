5de797b7f244cc3d95620323e7ad121ca8bd4913
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2011-10-03 17:38:42 +0000

    Working on GUI.

--- passwolk/src/main/webapp/js/passwolk.js
+++ passwolk/src/main/webapp/js/passwolk.js
@@ -1,48 +1,44 @@
 $(document).ready(function() {
   var accounts;
   
-  $('body').delegate('input[type=search]', 'keyup', function() {
+  $('div#accountEntry form').submit(doSubmit);
+  $('input[type=search]').keyup(function() {
     var searchValue = $(this).val();
+    console.log(searchValue);
     var filteredAccounts = $.grep(accounts, function(account) {
       return account.title.indexOf(searchValue) != -1;
     });
-    render('account_view', accounts);
+    render(accounts);
   });
-  
-  $.get('/tmpl/account_view.tmpl', function(markup) {
-    $.template('account_view', markup);
-    $.getJSON('/account', function(data) {
-      accounts = data;
-      render('account_view', accounts);
-    });
+  $.getJSON('/account', function(data) {
+    accounts = data;
+    render(accounts);
   });
 });
 
-function render(name, data) {
-  $('body').empty();
-  $.tmpl(name, {data: data}).appendTo('body');
+function render(data) {
+  $('div#accountList ul').empty();
+  $('#accountListTmpl').tmpl(data).appendTo('div#accountList ul');
 }
 
-function load(url) {
-  $('body').load(url, function() {
-    $('form').submit(function(event) {
-      alert('submit');
-      event.preventDefault();
-      var url = $(this).attr('action');
-      var method = $(this).attr('method');
-      var data = $(this).serialize();
-      console.log(url);
-      console.log(method);
-      console.log(data); 
-      $.ajax({
-        url : url,
-        type : method,
-        data : data,
-        success : function(data) {
-          alert('done');
-        }
-      });
-      return false;
-    });
+function doSubmit(event) {
+  alert('doSubmit');
+  var form = $(this);
+  event.preventDefault();
+  var url = form.attr('action');
+  var method = form.attr('method');
+  var data = form.serialize();
+  console.log(url);
+  console.log(method);
+  console.log(data); 
+  $.ajax({
+    url : url,
+    type : method,
+    data : data,
+    success : function(data) {
+      alert('done');
+      form.find('input[type=text]').val('');
+    }
   });
+  return false;
 }
