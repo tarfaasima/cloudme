4ec163f6dd154336b08a433f3c6c440b6a03a6f2
moritz@cloudme.org <moritz@cloudme.org@bf317f10-b32e-11dd-99fa-ddd771690bd5>
2012-03-27 05:31:04 +0000

    Added namespace to the NotepadController.

--- notepad/src/main/java/org/cloudme/notepad/rest/NotepadController.java
+++ notepad/src/main/java/org/cloudme/notepad/rest/NotepadController.java
@@ -1,12 +1,19 @@
 package org.cloudme.notepad.rest;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.cloudme.notepad.date.DateModule;
 import org.cloudme.notepad.export.ExportModule;
 import org.cloudme.notepad.meeting.MeetingModule;
 import org.cloudme.notepad.note.NoteModule;
 import org.cloudme.wrestle.Controller;
+
+import com.google.appengine.api.NamespaceManager;
+import com.google.appengine.api.users.UserServiceFactory;
 
 public class NotepadController extends Controller {
     @Override
@@ -16,5 +23,11 @@ public class NotepadController extends Controller {
         register(new NoteHandler());
         register(new TaskHandler());
         register(new TopicHandler());
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        NamespaceManager.set(UserServiceFactory.getUserService().getCurrentUser().getUserId());
+        super.service(req, resp);
     }
 }
